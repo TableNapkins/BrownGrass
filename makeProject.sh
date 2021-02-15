aed89d3683ae0c0b13bccf55474afc8256a26af0
gairsonm <gairsonm@mail.gvsu.edu>
2021-02-15 08:40:31 +0000

    Update makeProject.sh

--- makeProject.sh
+++ makeProject.sh
@@ -3,13 +3,32 @@
 
 make() {
 	
- mkdir "$1"
-
+ mkdir $1
+ cd $1
+ echo $1 >> README.md
+ echo $1 >> .gitingore
 }
 
-make 
+make $1
+
+mdir () {
+
+ mkdir bin include lib share man info 
+ 
+}
+mdir
+
+# git add $1
+# git commit -m "$2"
+
+
+
+
+
+
 
  
+
 
 
 
